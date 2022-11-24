b208d0088746f82d050cab2a410a4965e2cb5dd3
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-24 09:25:25 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1tv4Ml27E7TTEGjRq7w-D_XRi3rnLOVE",
+  authDomain: "used-mobile-resale.firebaseapp.com",
+  projectId: "used-mobile-resale",
+  storageBucket: "used-mobile-resale.appspot.com",
+  messagingSenderId: "436642492510",
+  appId: "1:436642492510:web:d168e60ee05f7d91d444cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
