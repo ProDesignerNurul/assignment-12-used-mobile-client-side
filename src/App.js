ac4bbb391bc3894477dfa1fdbcfec60f9baf6e4c
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-26 09:23:02 +0600

    remove all code for a big issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/Routes/Routes';
-import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className='lg:w-11/12 mx-auto'>
-      <RouterProvider router={router}></RouterProvider>
-      <Toaster />
+    <div className="App">
+      <h2>hello</h2>
     </div>
   );
 }
